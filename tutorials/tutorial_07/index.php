33873e9ff63402193f4ec845cf337b895a0b041a
YM4528 <yadanar.it.1999@gmail.com>
2021-12-21 14:59:29 +0800

    [master] added tuto_07

---
+++ tutorials/tutorial_07/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial_07</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php
+    $userInput = "";
+    session_start();
+    if (isset($_SESSION["userInput"])) {
+        $userInput = $_SESSION["userInput"];
+        session_unset();
+    }
+    ?>
+    <div class="con">
+        <h1>QRCode Generator</h1>
+        <form action="qrcode.php" method="post">
+            <label>Enter ID :</label>
+            <input type="text" name="userInput" id="userInput" value="<?= $userInput; ?>" required><br>
+            <input type="submit" value="Generate" name="generate">
+        </form>
+        <br>
+        <?php
+        if (isset($_GET["status"])) {
+            echo '<img src="image.png" />';
+        }
+        ?>
+    </div>
+</body>
+
+</html>
---
+++ tutorials/tutorial_07/qrcode.php
@@ -0,0 +1,16 @@
+<?php
+include('libraries/phpqrcode/qrlib.php');
+
+$userInput = filter_var($_POST["userInput"], FILTER_SANITIZE_STRING);
+session_start();
+$_SESSION["userInput"] = $userInput;
+ob_start("callback");
+$codeText = $userInput;
+$debugLog = ob_get_contents();
+ob_end_clean();
+
+//save img.png
+QRcode::png($codeText, "image.png", QR_ECLEVEL_H, 10);
+header("location:index.php?status=1");
+
+?>
