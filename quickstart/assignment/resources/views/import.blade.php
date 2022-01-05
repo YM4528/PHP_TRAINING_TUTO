8e5c0f3a254dbf0da3397b09ce72b85362de2c36
YM4528 <yadanar.it.1999@gmail.com>
2022-01-05 16:31:16 +0800

    [master] added laravel export&import data

--- quickstart/assignment/resources/views/import.blade.php
+++ quickstart/assignment/resources/views/import.blade.php
@@ -4,7 +4,7 @@
   <form action="{{ route('student.import.post') }}" method="post" enctype="multipart/form-data">
     @csrf
     <h2>Import</h2>
-    <p>Choose cvs file to import</p>
+    <p>Choose file to import</p>
     <input type="file" name="file" class="form-control">
     <br>
     <button class="btn btn-success">Import Student Data</button>
