747d30b8f65a61c631defc178773ad80620c9e96
YM4528 <yadanar.it.1999@gmail.com>
2022-01-05 16:39:27 +0800

    [master] added laravel basic task list

---
+++ quickstart/quickstart/app/Contracts/Dao/User/UserDaoInterface.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Contracts\Dao\User;
+
+use Illuminate\Http\Request;
+
+/**
+ * Interface for Data Accessing Object of Post
+ */
+interface UserDaoInterface
+{
+    /**
+     * To create new user
+     * @param Request $request request inputs
+     * @return Object $user
+     */
+    public function register(Request $request);
+
+    /**
+     * To login user
+     * @param Request $request request inputs
+     * @return true or false
+     */
+    public function login(Request $request);
+
+     /**
+     * To logout user
+     * @param
+     * @return
+     */
+    public function logout();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//namespace App\Contracts\Dao\User;
+//
+//use Illuminate\Http\Request;
+//
+///**
+// * Interface of Data Access Object for user
+// */
+//interface UserDaoInterface
+//{
+//  /**
+//   * To get user by id
+//   * @param string $id user id
+//   * @return Object $user user object
+//   */
+//  public function getUserById($id);
+//
+//  /**
+//   * To get user list
+//   * @return array $userList list of users
+//   */
+//  public function getUserList();
+//
+//  /**
+//   * To Update User with values from request
+//   * @param Request $request request including inputs
+//   * @return Object updated user object
+//   */
+//  public function updateUser(Request $request);
+//
+//  /**
+//   * To change user password
+//   * @param array $validated Validated values from request
+//   * @return Object $user user object
+//   */
+//  public function changeUserPassword($validated);
+//
+//  /**
+//   * To delete user by id
+//   * @param string $id user id
+//   * @param string $deletedUserId deleted user id
+//   * @return string $message message for success or not
+//   */
+//  public function deleteUserById($id, $deletedUserId);
+//
+//  /**
+//   * To save user that from api request
+//   * @param array $validated Validated values form request
+//   * @return Object created user object
+//   */
+//  public function saveUser($validated);
+//
+//  /**
+//   * To update user that from api request
+//   * @param array $validated Validated values form request
+//   * @return Object created user object
+//   */
+//  public function updateUserViaAPI($validated);
+//
+//  /**
+//   * To change user password API
+//   * @param array $validated Validated values from request
+//   * @return Object $user user object
+//   */
+//  public function changeUserPasswordAPI($validated);
+//}
